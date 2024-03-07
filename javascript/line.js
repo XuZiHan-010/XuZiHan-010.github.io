1b10eea9a8394502227346e6f514422fd04c7e03
XuZiHan-010 <146757205+XuZiHan-010@users.noreply.github.com>
2024-03-07 23:47:00 +0000

    Update line.js

--- javascript/line.js
+++ javascript/line.js
@@ -115,7 +115,7 @@ $.get(jsonDataPath, function (_rawData) {
      grid: {
       left: '5%', 
       right: '20%',
-      bottom: '15%',
+      bottom: '20%',
       top:'10%',
       width: 'auto',
       height: '80%',
