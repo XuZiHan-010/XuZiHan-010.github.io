2ee85bddc5f87e3caf4a027c1d65966c9043accc
XuZiHan-010 <146757205+XuZiHan-010@users.noreply.github.com>
2024-03-07 15:45:22 +0000

    Update midwestregion.js

--- javascript/midwestregion.js
+++ javascript/midwestregion.js
@@ -109,13 +109,13 @@ $.get(jsonDataPath, function (_rawData) {
             fontWeight: 'bold'
         }
     },
-       grid: {
+           grid: {
       left: '5%', 
-      right: '13%',
+      right: '20%',
       bottom: '5%',
       top:'10%',
-      width: '80%',
-      height: '90%',
+      width: 'auto',
+      height: 'auto',
       containLabel: true // This option ensures that the labels are contained within the grid
   },
       series: 
