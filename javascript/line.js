34eb8a0ed5791f446dbde713027eb5522e0e2585
XuZiHan-010 <146757205+XuZiHan-010@users.noreply.github.com>
2024-03-07 15:29:07 +0000

    Update line.js

--- javascript/line.js
+++ javascript/line.js
@@ -112,12 +112,12 @@ $.get(jsonDataPath, function (_rawData) {
             fontWeight: 'bold'
         }
     },
-    grid: {
+     grid: {
       left: '5%', 
-      right: '10%',
+      right: '13%',
       bottom: '5%',
       top:'10%',
-      width: '85%',
+      width: '80%',
       height: '90%',
       containLabel: true // This option ensures that the labels are contained within the grid
   },
