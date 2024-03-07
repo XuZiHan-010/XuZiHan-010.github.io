af59bd5e255b2fb5e1b25c1a9a534e541ddba98c
XuZiHan-010 <146757205+XuZiHan-010@users.noreply.github.com>
2024-03-07 21:40:01 +0000

    Update northeastregion.js

--- javascript/northeastregion.js
+++ javascript/northeastregion.js
@@ -109,7 +109,8 @@ $.get(jsonDataPath, function (_rawData) {
             fontWeight: 'bold'
         }
     },
-     left: '5%', 
+       grid: {
+      left: '5%', 
       right: '20%',
       bottom: '5%',
       top:'10%',
